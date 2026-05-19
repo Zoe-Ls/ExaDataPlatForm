ccfa1bd80d5c98ce6eb9af6329b8fa8c08f97a72
Zoe-Ls <z520zoe1213520zn@163.com>
2026-05-19 08:12:22 +0800

    Init

---
+++ ExaDataProject/src/main/java/com/hzau/Main.java
@@ -0,0 +1,12 @@
+package com.hzau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
