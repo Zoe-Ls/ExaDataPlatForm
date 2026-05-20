b7a2e70e9648ae1069c54c583e82be7e0ec06297
Zoe-Ls <z520zoe1213520zn@163.com>
2026-05-20 09:19:29 +0800

    feat(Main): 添加@MapperScan注解，让Mybatis能够扫描到Mapper
    - 添加@MapperScan("com.hzau.Mapper")到启动类
    - 能够自动扫描到Mapper接口

--- ExaDataProject/src/test/java/com/hzau/ApplicationTest.java
+++ ExaDataProject/src/test/java/com/hzau/ApplicationTest.java
@@ -1,15 +1,28 @@
 package com.hzau;
 
+import com.hzau.Domain.User;
+import com.hzau.Mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 public class ApplicationTest {
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     public void testJunit(){
         System.out.println(1);
         System.out.println("Spring Boot 版本: " + SpringBootVersion.getVersion());
+    }
+
+    @Test
+    public void testMapper(){
+        List<User> users = userMapper.findAll();
+        System.out.println(users);
     }
 }
